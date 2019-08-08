fa959b490141e15f9585815e40b508046d83d970
Thiago Bignotto <bignotto@gmail.com>
2019-08-08 00:41:32 -0300

    Security Fix and Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express =  require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 
 const app = express();
@@ -12,6 +13,7 @@ mongoose.connect(db_user,{
     useNewUrlParser: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
