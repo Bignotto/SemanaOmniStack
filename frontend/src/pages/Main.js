992d0ac357da4cca2773c0a94641360b7423674f
Thiago Bignotto <bignotto@gmail.com>
2019-08-12 02:00:39 -0300

    aula3: like dislike

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import api from '../services/api'
 
 import logo from '../assets/logo.svg'
@@ -7,94 +7,61 @@ import dislike from '../assets/dislike.svg'
 import './Main.css'
 
 export default function Main({ match }) {
+    const [users, setUsers] = useState([]);
+
     useEffect(() => {
         async function loadUsers() {
             const response = await api.get('/devs', {
                 headers: {user: match.params.id}
             })
 
-            console.log(response.data);
+            setUsers(response.data);
         }
 
         loadUsers();
 
-    }, [match.params.id])
+    }, [match.params.id]);
+
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: {user: match.params.id}
+        });
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleLike(id) {
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: {user: match.params.id}
+        });
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return (
         <div className="main-container">
             <img src={logo} alt=""/>
-            <ul>
-                <li>
-                    <img src="https://avatars3.githubusercontent.com/u/2911353?v=4" alt=""/>
-                    <footer>
-                        <strong>Thiago Bignotto</strong>
-                        <p>Desenvolvedor full stack fodástico!! Desenvolvedor full stack fodástico!! Desenvolvedor full stack fodástico!! Desenvolvedor full stack fodástico!! Desenvolvedor full stack fodástico!! Desenvolvedor full stack fodástico!!</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike"/>
-                        </button>
-                        <button type="button">
-                        <img src={like} alt="like"/>
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars3.githubusercontent.com/u/2911353?v=4" alt=""/>
-                    <footer>
-                        <strong>Thiago Bignotto</strong>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike"/>
-                        </button>
-                        <button type="button">
-                        <img src={like} alt="like"/>
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars3.githubusercontent.com/u/2911353?v=4" alt=""/>
-                    <footer>
-                        <strong>Thiago Bignotto</strong>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike"/>
-                        </button>
-                        <button type="button">
-                        <img src={like} alt="like"/>
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars3.githubusercontent.com/u/2911353?v=4" alt=""/>
-                    <footer>
-                        <strong>Thiago Bignotto</strong>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike"/>
-                        </button>
-                        <button type="button">
-                        <img src={like} alt="like"/>
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars3.githubusercontent.com/u/2911353?v=4" alt=""/>
-                    <footer>
-                        <strong>Thiago Bignotto</strong>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike"/>
-                        </button>
-                        <button type="button">
-                        <img src={like} alt="like"/>
-                        </button>
-                    </div>
-                </li>
-            </ul>
+            {users.length > 0 ? (
+                    <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <img src={user.avatar} alt={user.name}/>
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="dislike"/>
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="like"/>
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <div className="empty">Acabou :(</div>
+            )}
         </div>   
     );
 }
